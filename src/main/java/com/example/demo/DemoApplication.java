2302b3bf741c7e2f60acad94320d6511786c5613
Luis Rodriguez Fernandez <luis.rodriguezfernandez@sonarsource.com>
2025-03-17 10:28:06 +0100

    If else logic not covered and using system.out.println

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,10 +3,21 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Random;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		int min = 0;
+		int max = 9;
+		Random randomGenerator = new Random();
+		int random = randomGenerator.nextInt(max - min) + min;
+		if (random => 5) {
+		   System.out.println("Hello, low!");
+		} else {
+			System.out.println("Hello, up!");
+		}
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
